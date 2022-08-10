db3707ba4138b316adc7a177beb55d937862be89
arinn <arinn1250@gmail.com>
2022-08-09 20:53:01 -0700

    changed jmx port to environment variable

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM amazoncorretto:18
 COPY target/SimpleJMX-1.0-SNAPSHOT-jar-with-dependencies.jar /app/SimpleJMX.jar
 COPY start.sh /app/start.sh
 
+ENV JMX_PORT=9001
+
 CMD ["bash", "/app/start.sh"]
--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 java -jar \
   -Dcom.sun.management.jmxremote \
-  -Dcom.sun.management.jmxremote.port=9001 \
-  -Dcom.sun.management.jmxremote.rmi.port=9001 \
+  -Dcom.sun.management.jmxremote.port=$JMX_PORT \
+  -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.ssl=false \
   -Djava.rmi.server.hostname=0.0.0.0 \
