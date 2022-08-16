56846ce014f27f7132629f4d06976783fc4f055f
arinn <arinn1250@gmail.com>
2022-08-16 07:17:27 -0700

    added remote debugging port

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ java -jar \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.ssl=false \
   -Djava.rmi.server.hostname=0.0.0.0 \
+  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 \
   /app/SimpleJMX.jar
